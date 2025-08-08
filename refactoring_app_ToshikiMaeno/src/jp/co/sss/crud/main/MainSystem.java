a25517435e187a6a3fd5b4eb15be877cffaf54ae
MaenoKits0806 <toshiki.maeno@kits.co.jp>
2025-08-08 15:50:48 +0900

    コンソールに出力するメニューメッセージの内容のコード修正

--- refactoring_app_ToshikiMaeno/src/jp/co/sss/crud/main/MainSystem.java
+++ refactoring_app_ToshikiMaeno/src/jp/co/sss/crud/main/MainSystem.java
@@ -8,6 +8,7 @@ import java.sql.SQLException;
 import java.text.ParseException;
 
 import jp.co.sss.crud.db.EmployeeDAO;
+import jp.co.sss.crud.util.ConstantMsg;
 
 /**
  * 社員情報管理システム開始クラス 社員情報管理システムはこのクラスから始まる。<br/>
@@ -32,15 +33,7 @@ public class MainSystem {
 
 		do {
 			// メニューの表示
-			System.out.println("=== 社員管理システム ===");
-			System.out.println("1.全件表示");
-			System.out.println("2.社員名検索");
-			System.out.println("3.部署ID検索");
-			System.out.println("4.新規登録");
-			System.out.println("5.更新");
-			System.out.println("6.削除");
-			System.out.println("7.終了");
-			System.out.print("メニュー番号を入力してください：");
+			System.out.print(ConstantMsg.MENU_MESSAGE_ALL_DISPLAY);
 
 			// メニュー番号の入力
 			String menuNoStr = br.readLine();
@@ -55,7 +48,7 @@ public class MainSystem {
 
 			case 2:
 				// 社員名検索
-				System.out.print("社員名:");
+				System.out.print(ConstantMsg.MENU_MESSAGE_EMPLOYEE_NAME);
 
 				// 検索機能の呼出
 				EmployeeDAO.findEmployeeName();
@@ -63,7 +56,7 @@ public class MainSystem {
 
 			case 3:
 				// 検索する部署IDを入力
-				System.out.print("部署ID(1:営業部、2:経理部、3:総務部)を入力してください:");
+				System.out.print(ConstantMsg.MENU_MESSAGE_DEPTID_INPUT);
 				String deptId = br.readLine();
 				int deptIdInt = Integer.parseInt(deptId);
 
@@ -73,17 +66,17 @@ public class MainSystem {
 
 			case 4:
 				// 登録する値を入力
-				System.out.print("社員名:");
+				System.out.print(ConstantMsg.MENU_MESSAGE_EMPLOYEE_NAME);
 				String emp_name = br.readLine();
-				System.out.print("性別(0:その他, 1:男性, 2:女性, 9:回答なし):");
+				System.out.print(ConstantMsg.MENU_MESSAGE_GENERAL);
 				String Seibetsu = br.readLine();
 				int Seibetsu2 = Integer.parseInt(Seibetsu);
-				System.out.print("生年月日(西暦年/月/日):");
+				System.out.print(ConstantMsg.MENU_MESSAGE_BIRTHDAY);
 				String birthday = br.readLine();
 				String strDate = birthday.replaceAll("/", "-");
 				Date birthday2 = java.sql.Date.valueOf(strDate);
 				
-				System.out.print("部署ID(1:営業部、2:経理部、3:総務部):");
+				System.out.print(ConstantMsg.MENU_MESSAGE_DEPTID);
 				String deptIdInsert = br.readLine();
 				int deptIdInt2 = Integer.parseInt(deptIdInsert);
 
@@ -95,7 +88,7 @@ public class MainSystem {
 
 			case 5:
 				// 更新する社員IDを入力
-				System.out.print("更新する社員の社員IDを入力してください：");
+				System.out.print(ConstantMsg.MENU_MESSAGE_EMPLOYEE_UPDATE_INPUT);
 
 				// 更新する値を入力する
 				String empId_1 = br.readLine();
@@ -103,13 +96,13 @@ public class MainSystem {
 
 				// 更新機能の呼出
 				EmployeeDAO.update(empId_2);
-				System.out.println("社員情報を更新しました");
+				System.out.println(ConstantMsg.MENU_MESSAGE_EMPLOYEE_INFORMATION_UPDATE);
 
 				break;
 
 			case 6:
 				// 削除する社員IDを入力
-				System.out.print("削除する社員の社員IDを入力してください：");
+				System.out.print(ConstantMsg.MENU_MESSAGE_EMPLOYEE_DELETE_INPUT);
 				
 				// 更新する値を入力する
 				String empId_3 = br.readLine();
@@ -117,11 +110,13 @@ public class MainSystem {
 
 				// 削除機能の呼出
 				EmployeeDAO.delete(empId_4);
-				System.out.println("社員情報を削除しました");
+				System.out.println(ConstantMsg.MENU_MESSAGE_EMPLOYEE_INFORMATION_DELETE);
 				break;
 
 			}
 		} while (menuNo != 7);
-		System.out.println("システムを終了します。");
+		// システム終了
+		//System.out.println("システムを終了します。");
+		System.out.println(ConstantMsg.MENU_MESSAGE_SYSTEM_END);
 	}
 }
