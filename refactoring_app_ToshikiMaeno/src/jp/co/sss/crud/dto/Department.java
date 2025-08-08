7d90c302ade9db27e32683f424f4fa01c5662609
MaenoKits0806 <toshiki.maeno@kits.co.jp>
2025-08-08 18:41:28 +0900

    DTOパッケージの追加

---
+++ refactoring_app_ToshikiMaeno/src/jp/co/sss/crud/dto/Department.java
@@ -0,0 +1,8 @@
+package jp.co.sss.crud.dto;
+
+public class Department {
+	private Integer deptId;
+	private String deptName;
+	
+	
+}
---
+++ refactoring_app_ToshikiMaeno/src/jp/co/sss/crud/dto/Employee.java
@@ -0,0 +1,9 @@
+package jp.co.sss.crud.dto;
+
+public class Employee {
+	private Integer empId;
+	private String empName;
+	private Integer gender;
+	private String birthday;
+	private Department department;
+}
